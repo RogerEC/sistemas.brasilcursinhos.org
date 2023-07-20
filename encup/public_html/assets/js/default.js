ca98cb696f8149a6ff3dcd3f62062fb52ca7a3a5
Roger Eliodoro Condras <contato@rogerec.me>
2023-07-20 01:33:11 -0300

    Adição das informações de pagamento para inscrição ENCUP 2023

--- encup/public_html/assets/js/default.js
+++ encup/public_html/assets/js/default.js
@@ -6,4 +6,24 @@ $(function() {
             $(".main-content").addClass("d-none");
         }
     });
+
+    $("#btn-page-back").on("click", function(){
+        if($("#tickets").hasClass('d-none')) {
+            $("#pix").addClass("d-none");
+            $("#tickets").removeClass("d-none");
+        } else {
+            $("#btn-close-modal-subscription").trigger("click");
+        }
+    });
+
+    $(".btn-pay-with-pix").on("click", function(){
+        $("#tickets").addClass("d-none");
+        $("#pix").removeClass("d-none");
+        $(".code-pix").hide();
+        $("#code-pix-"+$(this).val()).show();
+    });
+
+    $(".code-pix-copy-paste").on("click", function(){
+        navigator.clipboard.writeText($(this).val());
+    });
 });
